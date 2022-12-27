9bbf9cbf501f306721f92317cbc40e30c8b662b6
Rawipa <110955515+nnrwp@users.noreply.github.com>
2022-12-27 16:08:48 +0700

    completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,10 +2,15 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double loan, rate, pay, PB, In, T, NB;
+	
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,26 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << fixed << setprecision(2);
+	int i = 1;
+	PB = loan;
+	NB = loan;
+	while(NB != 0){
+		cout << setw(13) << left <<  i; 
+		cout << setw(13) << left << PB;
+		In = PB*rate/100.0;
+		cout << setw(13) << left << In;
+		T = PB + In;
+		cout << setw(13) << left << T;
+		if(T <= pay) pay = T;
+		cout << setw(13) << left << pay;
+		NB = T - pay;
+		cout << setw(13) << left << NB;
+		cout << "\n";
+		PB = NB;
+		i++;
+		
+	}
+
 	return 0;
 }
